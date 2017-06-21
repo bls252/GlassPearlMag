0773d1d592130f83f78a33cc91c497a90f938c4a
bls252 <bls252@cornell.edu>
2017-06-21 00:40:35 -0400

    Finally fixed the article page!

---
+++ article.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<!-- This website can automatically format articles to html: https://wordtohtml.net/
+ -->
+<!-- This is how you do an apostrophe inside qoutes: &#8217;
+ -->
+<!-- Based off the article page at this URL:http://fashionmagazine.com/culture/the-national-ballet-of-canada-a-streetcar-named-desire/
+ -->
+<head>
+	<title>article</title>
+	<link rel="stylesheet" href="styling/article.css">
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+	<link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans|Quicksand:300" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <!-- js functions require Ajax -->
+    <script type="text/javascript" src="javascript/article.js"></script>
+	<body>
+
+		<?php include 'includes/header.php';?>
+
+		<div id="title_photo_wrapper">
+			<img id="title_photo" src="images/read1.jpg" alt="fashion_model">
+		</div>
+		
+		<div id="content">
+
+			<div id="main_article">
+
+				<h3 id="photo_credit"> Photography via @PersonOnInstagram </h3>
+
+				<hr id="decorative_line">
+
+		        <h1 class="articleHeadline" style="font-family: 'Playfair Display', serif; font-size: 2.3em">Simple is Fashionable: Notes on Grace &#38; Elegance</h1>
+
+		        <p class="article-subline" style="font-family: 'Lato', sans-serif; font-size: 1.7em;">Fit for a princess—sans royal price tag.</p>
+
+		        <p class="article-text" style="font-family: 'Lato', sans-serif; font-size: 1.3em; line-height: 200%;">&emsp;&emsp;&emsp;Eget quam vestibulum consectetuer, lacus semper lorem in sodales duis. A orci porttitor lacinia condimentum phasellus nec, mi morbi semper aliquet wisi consectetuer, consectetur nunc morbi libero a. Nec vitae in et, id sit, vestibulum curabitur pede aenean ridiculus massa quam, lorem amet porttitor, erat venenatis amet quam donec nulla. Morbi magna natoque et a, laoreet curabitur justo mauris nulla vestibulum, nulla faucibus, aenean turpis eget lacus facilisi nibh, in elit sem. Praesent ultricies auctor, elit morbi neque non ultrices, lacus amet eget, quam sed consequat nisl sed ligula porta. Tristique proin elementum pellentesque, nec at curabitur cras lacus potenti ut, nec enim. Nam porttitor. Vitae nunc lorem massa amet rutrum et. Pharetra aliquam proin, ornare commodo phasellus, nam mauris tincidunt, odio mattis, metus lectus neque augue vitae lectus. Tellus rutrum aenean, dictum fusce eget, mattis enim adipiscing, erat fringilla ipsum a, tincidunt rutrum quis nascetur cubilia. Urna class quis vel, neque metus et tortor ultrices pede, ipsum vehicula sed sed, tincidunt felis, elit bibendum diam tellus mollis faucibus ad. Vel felis felis semper mauris dui eu, a sed.</p>
+            </div>
+		</div>
+
+
+		<?php include 'includes/footer.php';?>
+
+
+	</body>
+</html>
